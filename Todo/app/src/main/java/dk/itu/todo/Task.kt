fa2eea07aebe1dacd0d27227ea870ad53ca25d1c
raar <raar@itu.dk>
2025-04-03 15:22:02 +0200

    Made "layout.xml" Made "task.xml" Created button and recycleView

---
+++ Todo/app/src/main/java/dk/itu/todo/Task.kt
@@ -0,0 +1,6 @@
+package dk.itu.todo
+
+data class Task(
+    val title: String,
+    val description: String,
+)
--- Todo/app/src/main/java/dk/itu/todo/ui/theme/Color.kt
+++ Todo/app/src/main/java/dk/itu/todo/ui/theme/Color.kt
@@ -9,3 +9,17 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+
+val Red = Color(0xFFFF0000)
+val Green = Color(0xFF00FF00)
+val Blue = Color(0xFF0000FF)
+
+val Cyan = Color(0xFF00FFFF)
+val Magenta = Color(0xFFFF00FF)
+val Yellow = Color(0xFFFFFF00)
+
+val teal200 = Color(0xFF03DAC5)
+val teal700 = Color(0xFF018786)
